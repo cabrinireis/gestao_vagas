a51ec3c2dfbd1cb0887b8f650dd1d3faa1b82941
Cabrini <cabrinireis@gmail.com>
2025-07-03 15:01:16 +0100

    validation user in database

--- src/main/java/cabrini/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/cabrini/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,17 +1,28 @@
 package cabrini.gestao_vagas.modules.candidate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
 
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@Entity(name = "tb_candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String name;
 
     @NotBlank
@@ -22,7 +33,11 @@ public class CandidateEntity {
     private String email;
 
     private String password;
+    @Length(min = 10, max = 100, message = "The text must be between 10 and 100 characters long.")
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
--- src/main/java/cabrini/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/cabrini/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,7 +1,10 @@
 package cabrini.gestao_vagas.modules.candidate.controllers;
 
+import cabrini.gestao_vagas.exceptions.UserFoundException;
 import cabrini.gestao_vagas.modules.candidate.CandidateEntity;
+import cabrini.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,10 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CandidateRepository candidateRepository;
+
     @PostMapping("/")
     public ResponseEntity<Void> create(@RequestBody @Valid CandidateEntity candidateEntity) {
-        System.out.println(candidateEntity.getUsername());
-        System.out.println(candidateEntity.getEmail());
-        return ResponseEntity.ok().build();
+        this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail()).ifPresent(user -> {
+            throw new UserFoundException();
+        });
+        return ResponseEntity.badRequest().build();
     }
 }

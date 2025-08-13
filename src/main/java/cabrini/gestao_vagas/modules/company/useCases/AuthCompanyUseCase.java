f99930f2dcf1b276f352fe4d7a14ce5d6d4d2759
Cabrini <cabrinireis@gmail.com>
2025-08-13 15:18:02 +0100

    refactor company filter

--- src/main/java/cabrini/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/cabrini/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,6 +1,7 @@
 package cabrini.gestao_vagas.modules.company.useCases;
 
 import cabrini.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import cabrini.gestao_vagas.modules.company.dto.AuthCompanyResponseDTO;
 import cabrini.gestao_vagas.modules.company.repositories.CompanyRepository;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 import javax.naming.AuthenticationException;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Arrays;
 
 @Service
 public class AuthCompanyUseCase {
@@ -26,7 +28,7 @@ public class AuthCompanyUseCase {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+    public AuthCompanyResponseDTO execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
         var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(() -> {
             throw new UsernameNotFoundException("Username/password incorrect");
         });
@@ -37,11 +39,21 @@ public class AuthCompanyUseCase {
         }
 
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
-        var token = JWT.create().withIssuer("javagas")
-                .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
-                .withSubject(company.getId().toString()).sign(algorithm);
 
-        return token;
+        var expiresIn = Instant.now().plus(Duration.ofMinutes(10));
+
+        var token = JWT.create().withIssuer("javagas")
+                .withExpiresAt(expiresIn)
+                .withSubject(company.getId().toString())
+                .withClaim("roles", Arrays.asList("COMPANY"))
+                .sign(algorithm);
+
+        var authCompanyResponseDTO = AuthCompanyResponseDTO.builder()
+                .access_token(token)
+                .expires_in(expiresIn.toEpochMilli())
+                .build();
+        
+        return authCompanyResponseDTO;
     }
 
 }
